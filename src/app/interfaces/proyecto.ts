5a94dfca3c68decd64468b048dc5797f9fd07365
Pablo Mariño Boga <pablo.marino.boga@gmail.com>
2025-03-06 15:55:35 +0100

    Update landing

--- src/app/interfaces/proyecto.ts
+++ src/app/interfaces/proyecto.ts
@@ -1,3 +1,24 @@
+export interface Evento {
+    fecha:             string;
+    fecha_de_creacion: string;
+    id_evento:         number;
+    titulo_es:         string;
+    titulo_gl:         string;
+    web:               string;
+}
+export interface Imagen {
+    alt_text_es:  string;
+    alt_text_gl:  string;
+    id_imagen:    number;
+    id_proyecto:  number;
+    ruta:         string;
+}
+export interface Participacion {
+    id_evento:    number;
+    id_proyecto:  number;
+    resultado_es: string;
+    resultado_gl: string;
+}
 export interface Proyecto {
     fecha_de_creacion: Date;
     id_autor:          number;
@@ -10,6 +31,13 @@ export interface Proyecto {
     titulo_gl:         string;
     video:             string;
     web:               string;
+    apellido1:         string;
+    apellido2:         string;
+    nombre:            string;
+    imagen_autor:      string;
+    eventos:           Evento[];
+    imagenes:          Imagen[];
+    participaciones:   Participacion[];
 }
 
 // necesito Imagen thumbnail, alt text, titulo, resumen, enlaces, nombre usuario, participaciones
